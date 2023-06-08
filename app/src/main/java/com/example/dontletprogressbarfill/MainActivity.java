e145d41f8c23092abc99c8ae4690895343ef41bf
shakthishamruth <shakthishamruth@gmail.com>
2023-06-08 22:42:43 +0530

    Added Progress Bar

--- app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
+++ app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
@@ -3,24 +3,65 @@ package com.example.dontletprogressbarfill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button Left, Right;
 
+    private int fill = 0;
+
+    private ProgressBar progressBar;
+
+    private boolean gameOver = false, running = false;
+
+    private TextView gameOverText;
+
+    private Thread thread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        gameOverText = findViewById(R.id.gameOverText);
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (running) {
+                    if (!gameOver) {
+                        fill = fill + 10;
+                        progressBar.setProgress(fill);
+                    }
+                    if (fill >= 100 && !gameOver) {
+                        gameOver = true;
+                        progressBar.setVisibility(View.INVISIBLE);
+                        gameOverText.setVisibility(View.VISIBLE);
+                        return;
+                    }
+                    SystemClock.sleep(500);
+                }
+            }
+        });
     }
 
     public void onClickLeft(View view) {
+        if (!running) {
+            running = true;
+            thread.start();
+        }
         Left = findViewById(R.id.leftButton);
         Right = findViewById(R.id.rightButton);
         Right.setEnabled(true);
         Left.setEnabled(false);
+        fill = fill - 10;
     }
 
     public void onClickRight(View view) {
@@ -28,5 +69,6 @@ public class MainActivity extends AppCompatActivity {
         Right = findViewById(R.id.rightButton);
         Right.setEnabled(false);
         Left.setEnabled(true);
+        fill = fill - 10;
     }
 }
