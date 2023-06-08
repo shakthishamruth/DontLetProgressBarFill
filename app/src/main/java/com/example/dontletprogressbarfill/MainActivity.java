b538908e0428eba8c802672cd9f3dbb0aeaacc5d
shakthishamruth <shakthishamruth@gmail.com>
2023-06-08 16:23:08 +0530

    Created Button

--- app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
+++ app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.dontletprogressbarfill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Left, Right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLeft(View view) {
+        Left = findViewById(R.id.leftButton);
+        Right = findViewById(R.id.rightButton);
+        Right.setEnabled(true);
+        Left.setEnabled(false);
+    }
+
+    public void onClickRight(View view) {
+        Left = findViewById(R.id.leftButton);
+        Right = findViewById(R.id.rightButton);
+        Right.setEnabled(false);
+        Left.setEnabled(true);
     }
 }
