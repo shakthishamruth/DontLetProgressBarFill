5c202bfa54f31e10ecdab2f553943b79dcf687d6
shakthishamruth <shakthishamruth@gmail.com>
2023-06-10 17:43:28 +0530

    Update v0.1

--- app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
+++ app/src/main/java/com/example/dontletprogressbarfill/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickPlay(View view) {
-        setContentView(R.layout.maingame);
+        //setContentView(R.layout.maingame);
     }
 
     public void onClickLeftMain(View view) {
